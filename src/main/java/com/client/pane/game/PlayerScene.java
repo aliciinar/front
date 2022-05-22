6935303e3459ac5a8f026132ab3acd8d5a795e58
Saitss <saitsutss@gmail.com>
2022-05-22 18:25:16 +0300

    Updated the Game.

--- src/main/java/com/client/pane/game/PlayerScene.java
+++ src/main/java/com/client/pane/game/PlayerScene.java
@@ -1,8 +1,29 @@
 package com.client.pane.game;
 
 public class PlayerScene implements  IPrepareScene{
+
+
+
     @Override
-    public void prepareScene(GameBoard gameBoard) {
-        gameBoard.activationRollButton(false);
+    public void playerWaitRoll(GameBoard gameBoard) {
+
+        gameBoard.activateButtons(false , true , true);
+    }
+
+    @Override
+    public void playerWaitPurchasable(GameBoard gameBoard) {
+        gameBoard.activateButtons(true , false , false);
+    }
+
+    @Override
+    public void PlayerWaitNotPurchasable(GameBoard gameBoard) {
+        gameBoard.activateButtons(true , true , false);
+    }
+
+
+    @Override
+    public void playerEnd(GameBoard gameBoard) {
+
+        gameBoard.activateButtons(true , true , true);
     }
 }
